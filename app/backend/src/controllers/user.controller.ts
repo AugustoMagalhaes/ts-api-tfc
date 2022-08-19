66aa1611f6972db0ccf57f13096dda5c852eba9c
AugustoMagalhaes <augustommg@gmail.com>
2022-08-19 17:49:18 -0300

    chore: altera estrutura do UserController pra melhor organizaçao.

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -5,13 +5,18 @@ import UserService from '../services/user.service';
 class UserController {
   public static async login(req: Request, res: Response) {
     const { email, password } = req.body;
-    console.log(req.body);
-    const login = await UserService.login(email, password);
 
-    const token = TokenHandler.handleToken({ email, password });
+    try {
+      const login = await UserService.login(email, password);
 
-    if (login) {
+      const token = TokenHandler.handleToken({ email, password });
+
+      if (!login) {
+        return res.status(404).json({ message: 'User not found' });
+      }
       return res.status(200).json({ token });
+    } catch (err) {
+      return res.status(500).json({ message: 'Something went wrong' });
     }
   }
 }
