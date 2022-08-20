c4929292c0dcd72d0a953a62a08d81a9008dc53e
AugustoMagalhaes <augustommg@gmail.com>
2022-08-20 16:11:12 -0300

    feat: adiciona validaçoes no controller por mais estranho q isso seja.

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import TokenHandler from '../middlewares/token.handler';
+import Validations from '../middlewares/validations';
 import UserService from '../services/user.service';
 
 class UserController {
@@ -7,12 +8,18 @@ class UserController {
     const { email, password } = req.body;
 
     try {
+      const validationError = await Validations.loginValidation(email, password);
+
+      if (validationError) {
+        return res.status(400).json({ message: validationError });
+      }
+
       const login = await UserService.login(email, password);
 
       const token = TokenHandler.handleToken({ email, password });
 
       if (!login) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(401).json({ message: 'Incorrect email or password' });
       }
       return res.status(200).json({ token });
     } catch (err) {
