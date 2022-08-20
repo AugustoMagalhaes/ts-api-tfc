a58b63e9baff76e735befff9f7069a533187f672
AugustoMagalhaes <augustommg@gmail.com>
2022-08-20 03:30:21 -0300

    chore: altera loginValidation para arrow function pra evitar erro.

--- app/backend/src/middlewares/validations.ts
+++ app/backend/src/middlewares/validations.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
 class Validations {
-  public static loginValidation(req: Request, res:Response, next: NextFunction) {
+  public static loginValidation = (req: Request, res:Response, next: NextFunction) => {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     next();
-  }
+  };
 }
 
 export default Validations;
