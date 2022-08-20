f6b0ce28e7ff41b322d4121f9850c1c4415217b9
AugustoMagalhaes <augustommg@gmail.com>
2022-08-20 02:33:29 -0300

    feat: implementa validaçao de password e altera nome da funçao de validaçao pra validaçao de login em geral.

--- app/backend/src/middlewares/validations.ts
+++ app/backend/src/middlewares/validations.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 class Validations {
-  public static emailValidation(req: Request, res:Response, next: NextFunction) {
-    const { email } = req.body;
-    if (!email) return res.status(400).json({ message: 'All fields must be filled' });
+  public static loginValidation(req: Request, res:Response, next: NextFunction) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
     next();
   }
 }
