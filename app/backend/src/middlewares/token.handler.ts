ca3f04c4da15f75be9bc57bb3381ff63515708d2
AugustoMagalhaes <augustommg@gmail.com>
2022-08-21 01:40:59 -0300

    feat: implementa decodeToken em TokenHandler.

--- app/backend/src/middlewares/token.handler.ts
+++ app/backend/src/middlewares/token.handler.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config';
 import * as Jwt from 'jsonwebtoken';
 import { SignOptions } from 'jsonwebtoken';
+import UserModel from '../database/models/user.model';
 import ILoginUser from '../interfaces/loginUser.interface';
+import IUser from '../interfaces/user.interface';
 
 export default class TokenHandler {
   public static handleToken(loginUser: ILoginUser) {
@@ -11,5 +14,19 @@ export default class TokenHandler {
     };
     const token = Jwt.sign({ data: loginUser }, secret, jwtConfig);
     return token;
+  }
+
+  public static async decodeToken(token: string): Promise<IUser | boolean> {
+    const secret = process.env.JWT_SECRET as string;
+    const decoded = Jwt.verify(token, secret) as Jwt.JwtPayload;
+
+    const { email } = decoded.data;
+
+    const user = await UserModel.findOne({
+      where: { email },
+    });
+
+    if (!user) return false;
+    return user;
   }
 }
