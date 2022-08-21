cd9efdefef5ab15969efe8d0333275828b5a7fc5
AugustoMagalhaes <augustommg@gmail.com>
2022-08-21 01:42:16 -0300

    feat: adiciona rota '/validate' em userRoute.

--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -3,6 +3,7 @@ import UserController from '../controllers/user.controller';
 
 const router = Router();
 
+router.get('/validate', UserController.validate);
 router.post('/', UserController.login);
 
 export default router;
