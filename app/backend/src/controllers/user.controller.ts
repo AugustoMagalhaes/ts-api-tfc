42b160d0b68f8a6761aa28fd650adc751bd82267
AugustoMagalhaes <augustommg@gmail.com>
2022-08-21 01:41:39 -0300

    feat: implementa controller validate() para validaçao de token na rota.

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -26,6 +26,22 @@ class UserController {
       return res.status(500).json({ message: 'Something went wrong' });
     }
   }
+
+  public static async validate(req:Request, res:Response) {
+    const { authorization } = req.headers;
+
+    try {
+      const token = authorization as string;
+
+      const user = await UserService.validate(token);
+
+      const { role } = user;
+      if (!role) return res.status(404).json({ message: 'Invalid token' });
+      return res.status(200).json({ role });
+    } catch (err) {
+      return res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
 }
 
 export default UserController;
