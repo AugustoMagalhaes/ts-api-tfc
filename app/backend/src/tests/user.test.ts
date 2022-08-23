7cf66d36c56059b8a8b068eacc3648caa2294403
AugustoMagalhaes <augustommg@gmail.com>
2022-08-23 10:15:17 -0300

    test: implementa testes de casos falhos da rota '/login'.

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -13,29 +13,23 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', async () => {
+describe('Faz login com sucesso com os dados válidos', async () => {
   let chaiHttpResponse: Response;
 
   const body = {
-    "email": "admin@admin.com",
-    "password": "secret_admin"
+    email: 'admin@admin.com',
+    password: 'secret_admin',
   };
 
-   before(async () => {
-     sinon
-       .stub(UserModel, "findOne")
-       .resolves(
-         userAdminEmail as UserModel);
+  before(async () => {
+    sinon.stub(UserModel, 'findOne').resolves(userAdminEmail as UserModel);
 
-  chaiHttpResponse = await chai
-    .request(app)
-    .post('/login')
-    .send(body);
-   });
+    chaiHttpResponse = await chai.request(app).post('/login').send(body);
+  });
 
-  after(()=>{
+  after(() => {
     (UserModel.findOne as sinon.SinonStub).restore();
-  })
+  });
 
   it('Testa se retorna status 200 na rota "/login" com body valido', async () => {
     expect(chaiHttpResponse).to.have.status(200);
@@ -44,5 +38,58 @@ describe('Seu teste', async () => {
   it('Retorna message com token correto', async () => {
     expect(chaiHttpResponse.body).to.have.property('token');
   });
-
 });
+
+describe('Retorna mensagem de erro quando falta email ou password', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon.stub(UserModel, 'findOne').resolves(null);
+  });
+
+  after(() => {
+    (UserModel.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Quando falta email', async () => {
+    const body = { password: 'secret_admin' };
+
+    chaiHttpResponse = await chai.request(app).post('/login').send(body);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body).to.have.property('message', 'All fields must be filled');
+  });
+
+  it('Quando falta password', async () => {
+    const body = { email: 'admin@admin.com' };
+
+    chaiHttpResponse = await chai.request(app).post('/login').send(body);
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body).to.have.property('message', 'All fields must be filled');
+  });
+});
+
+describe('Retorna mensagem de erro quando usuario não está cadastrado', async () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon.stub(UserModel, 'findOne').resolves(null);
+  });
+
+  after(() => {
+    (UserModel.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Usuário não consta no banco de dados', async () => {
+    const body = {
+      email: 'admin@admin.com.br',
+      password: 'secret_admin'
+    }
+
+    chaiHttpResponse = await chai.request(app).post('/login').send(body);
+
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.have.property('message', 'Incorrect email or password');
+  })
+})
