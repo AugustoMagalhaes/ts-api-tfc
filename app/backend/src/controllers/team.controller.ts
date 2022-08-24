eb113007db6fb986190e512a1fbfc71b4094a37f
AugustoMagalhaes <augustommg@gmail.com>
2022-08-24 11:23:38 -0300

    feat: implementa TeamController.getTeamById.

--- app/backend/src/controllers/team.controller.ts
+++ app/backend/src/controllers/team.controller.ts
@@ -11,6 +11,21 @@ class TeamController {
       return res.status(500).json({ message: 'Something went wrong' });
     }
   }
+
+  public static async getTeamById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const numberId = Number(id);
+
+      const team = await TeamService.getTeamById(numberId);
+
+      if (!team) return res.status(404).json({ message: 'Team not found' });
+
+      return res.status(200).json(team);
+    } catch (err) {
+      if (err instanceof Error) return res.status(500).json({ message: err.message });
+    }
+  }
 }
 
 export default TeamController;
