694091705a747a728a8a13f105366c79d4ff85e7
AugustoMagalhaes <augustommg@gmail.com>
2022-08-25 15:47:42 -0300

    feat: implementa rota POST em '/matches'.

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -4,5 +4,6 @@ import MatchController from '../controllers/match.controller';
 const router = Router();
 
 router.get('/', MatchController.getAllTeams);
+router.post('/', MatchController.createMatch);
 
 export default router;
