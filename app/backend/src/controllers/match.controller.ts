e2ad90c0cc28b146dcd4f6e547499e61fcf648f3
AugustoMagalhaes <augustommg@gmail.com>
2022-08-25 15:39:29 -0300

    feat: implementa MatchController.createMatch.

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -11,6 +11,25 @@ class MatchController {
       return res.status(500).json({ message: 'Something went wrong' });
     }
   }
+
+  public static async createMatch(req: Request, res: Response) {
+    try {
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
+      const { authorization: token } = req.headers;
+
+      const validation = await MatchService.validate(token as string);
+
+      if (!validation) throw new Error('Invalid token');
+
+      const newMatch = await MatchService
+        .createMatch(homeTeam, homeTeamGoals, awayTeam, awayTeamGoals);
+
+      return res.status(201).json(newMatch);
+    } catch (err) {
+      console.log(err);
+      return err instanceof Error && res.status(500).json({ message: err.message });
+    }
+  }
 }
 
 export default MatchController;
