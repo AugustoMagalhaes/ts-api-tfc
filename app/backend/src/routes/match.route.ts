ea34f2bb59df0a2bbbbd6a98d2ce4107381f6639
AugustoMagalhaes <augustommg@gmail.com>
2022-08-25 21:06:27 -0300

    feat: adiciona rota PATCH 'matches/:id/finish'.

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', MatchController.getAllTeams);
 router.post('/', MatchController.createMatch);
+router.patch('/:id/finish', MatchController.finishMatch);
 
 export default router;
